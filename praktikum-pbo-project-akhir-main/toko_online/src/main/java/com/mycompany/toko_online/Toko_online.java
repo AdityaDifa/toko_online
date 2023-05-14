d6a10e45f53f84dac2652c624543c8e5dc52611e
Aditya Difa <adityadifa123@gmail.com>
2023-05-14 20:14:32 +0700

    pertama

---
+++ praktikum-pbo-project-akhir-main/toko_online/src/main/java/com/mycompany/toko_online/Toko_online.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.toko_online;
+
+import view.RiwayatView;
+
+/**
+ *
+ * @author USER 10
+ */
+public class Toko_online {
+    public static void main(String[] args) {
+        RiwayatView riwayatview = new  RiwayatView();
+        riwayatview.setVisible(true);
+    }
+}
